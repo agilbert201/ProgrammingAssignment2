9f05ba53fbb3d13c95c3ca40b97d2dfe0f8aae0c
Andrew Gilbert <andy@agilbert.net>
2014-12-21 12:11:24 -0500

    works with unit tests

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## These functions provide clients the ability to create a special
+## matrix that can cache it's inverse. Use 'makeCacheMatrix' to 
+## create the cached matrix, and 'cacheSolve' as an interface 
+## to get it's inverse.
+##
+## Example:
+##  m <- matrix(c(1,2,3,4),2,2)
+##  cm <- makeCacheMatrix(m)
+##  for(i in 1:10)
+##      cacheSolve(cm)
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## 'makeCacheMatrix' creates a special matrix which allows
+    ## clients to cache it's inverse
+    ##
+    ## Args:
+    ##  x: optionally specify the matrix, note assumed to be invertible
+    ##    
+    ## Returns:
+    ##  A list with functions to get,set the matrix and get,set it's inverse
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(theInv) inv <<- theInv
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## 'cacheSolve' returns the inverse of 'x'
+    ##
+    ## Args:
+    ## x: the special matrix x, note must be created through makeCacheMatrix
+    ##
+    ## Returns:
+    ##  the inverse of x
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    message("solving and caching new inverse")
+    data <- x$get()
+    theInv <- solve(data)
+    x$setinv(theInv)
+    theInv
 }
---
+++ tests/test-cachematrix.R
@@ -0,0 +1,34 @@
+## Note I don't yet enough knowledge of R or testthat
+## to testing caching more elegantly than message checks
+test_that("cacheMatrix returns accurate inverse",
+{
+    m1 <- matrix(c(1,2,3,4),2,2)
+    m1inv <- solve(m1)
+    cm <- makeCacheMatrix(m1)
+    expect_message(inv <- cacheSolve(cm), "solving and caching new inverse")
+    expect_identical(inv, m1inv)
+    ## run again to ensure still this way from cache
+    expect_message(inv <- cacheSolve(cm), "getting cached data")
+    expect_identical(inv, m1inv)
+})
+
+test_that("cacheMatrix caches all n+1 queries",
+{
+    m1 <- matrix(c(1,2,3,4),2,2)
+    cm <- makeCacheMatrix(m1)
+    expect_message(cacheSolve(cm), "solving and caching new inverse")
+    expect_message(cacheSolve(cm), "getting cached data")
+})
+
+test_that("cacheMatrix matrix change resets cache",
+{
+    m1 <- matrix(c(1,2,3,4),2,2)
+    cm <- makeCacheMatrix(m1)
+    expect_message(cacheSolve(cm), "solving and caching new inverse")
+    expect_message(cacheSolve(cm), "getting cached data")
+    m2 <- matrix(c(4,5,6,7),2,2)
+    cm$set(m2)
+    expect_message(cacheSolve(cm), "solving and caching new inverse")
+    expect_message(cacheSolve(cm), "getting cached data")
+})
+
